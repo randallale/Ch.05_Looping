884c8f98fe417feb5c88913319639aa7e3f6588d
Randallale <randallale@urbandaleschools.com>
2019-01-10 20:42:16 -0600

    Almost Done

--- 5.2_Roshambo.py
+++ 5.2_Roshambo.py
@@ -12,15 +12,34 @@ When the user quits print a win/loss record
 '''
 
 import random
-a = random.randrange(1,4)
 quit = "n"
 while quit == "n":
+    answer = input("Rock, Paper, or Scissors? Or Random? \n\r")
+    if answer.lower() == "random": #Random generator
+        answer = random.randrange(1,4)
+        if answer == 1:
+            answer = "Rock"
+        elif answer == 2:
+            answer = "Paper"
+        elif answer == 3:
+            answer = "Scissors"
+    a = random.randrange(1, 4) # Makes other "player's" choice
     if a == 1:
-        print("Rock")
+        a = "Rock"
     elif a == 2:
-        print("Paper")
+        a = "Paper"
     elif a == 3:
-        print("Scissors")
+        a = "Scissors"
+    print(answer,"vs.",a )
+    if answer.lower() == "rock" and a.lower() == "rock": #Checks both answers to see who wins
+        print("Tie")
+    elif answer.lower() == "scissors" and a.lower() == "scissors":
+        print ("Tie")
+    quit = input("Would you like to quit? \n\r")
+    if quit.lower() == ("no"):
+        quit = "n"
+    elif quit.lower() == ("yes"):
+        quit = "y"
 
 
 
