e543663fe65c62d545ec76692dddf6e82a4a52bc
Alex <randallale@urbandaleschools.com>
2019-01-29 23:14:39 -0600

    Tested a lot and fixed a few bugs

--- Better_Camel_Game.py
+++ Better_Camel_Game.py
@@ -13,17 +13,18 @@ natives_traveled = -20
 drinks = 6
 instructions = int(input("Would you like instructions?\n\r1: Yes\n\r2: No\n\r"))
 if instructions == 1: #Are instructions
-    print("Welcome to Camel. The object is to travel 200 miles across the great Gobi Desert.")
-    print("A tribe of knocked kneed pygmies will be chasing you.")
+    print("Welcome to the Millennium Falcon. It's the ship that made the Kessel run in less than 12 parsecs.")
+    print("The object is to travel 200 Solar Systems across the Galaxy to safety.")
+    print("A squad of imperial troopers will be chasing you.")
     print("You will be asked for commands every so often.")
 while not done: #Main loop
 
     #Prints Commands
     print("\n\t\tCommands")
-    print("A. Drink from your canteen.")
-    print("B. Ahead moderate speed.")
-    print("C. Ahead full speed")
-    print("D. Stop for the night")
+    print("A. Drink your supply of green milk.")
+    print("B. Move at a moderate speed.")
+    print("C. Punch it Chewie")
+    print("D. Repair the Millennium Falcon")
     print("E. Status check.")
     print("Q. Quit")
     user_choice = input("\n\rWhat is your command?\n\r")
@@ -34,27 +35,27 @@ while not done: #Main loop
         print("Goodbye!")
         break
     elif user_choice.upper() == "E":
-        print("Miles Traveled: ",miles_traveled)
-        print("Drinks in canteen: ",drinks)
-        print("The natives are",(miles_traveled-natives_traveled),"miles behind you")
+        print("Solar systems traveled: ",miles_traveled)
+        print("Drinks on the Millennium Falcon: ",drinks)
+        print("The Imperials are",(miles_traveled-natives_traveled),"Solar Systems behind you")
 
     elif user_choice.upper() == "D":
         camel_tiredness = 0
         natives_traveled += random.randrange(7,15)
         if not natives_traveled >= miles_traveled:
-            print("Your camel is happy")
+            print("The Millennium Falcon hyperdrive is fully repaired")
 
 
     elif user_choice.upper() == "C":
         miles_traveled += random.randrange(10,21)
-        print("You have traveled",miles_traveled,"miles")
+        print("You have traveled",miles_traveled,"solar systems")
         thirst += 1
         camel_tiredness += random.randrange(1,4)
         natives_traveled += random.randrange(7,15)
 
     elif user_choice.upper() == "B":
         miles_traveled += random.randrange(5,13)
-        print("You have traveled",miles_traveled,"miles")
+        print("You have traveled",miles_traveled,"solar systems")
         thirst += 1
         camel_tiredness += 1
         natives_traveled += random.randrange(7, 15)
@@ -64,34 +65,34 @@ while not done: #Main loop
             drinks -= 1
             thirst = 0
         else:
-            print("You don't have any drinks left in your canteen.")
+            print("You don't have green milk left on the ship")
 
     #Checking players stats
 
     if thirst > 6:
-        print("You died of thirst :(")
+        print("You died of thirst")
         break
     elif thirst > 4 and not natives_traveled >= miles_traveled and not camel_tiredness > 8:
-        print("You are thirsty.")
+        print("You are thirsty for some green milk.")
 
     if camel_tiredness > 8 and not thirst > 6:
-        print("Your camel is dead.")
+        print("The Millennium Falcon was destroyed")
         break
 
     elif camel_tiredness > 5 and not thirst > 6 and not natives_traveled >= miles_traveled:
-        print("Your camel is getting tired")
+        print("The Millennium Falcon's hyperdrive is starting to go bad")
 
     if natives_traveled >= miles_traveled and not miles_traveled >= 200:
-        print("The natives caught you!")
+        print("The Imperials caught you!")
         break
     elif miles_traveled >= 200:
         print("You won the game!")
         break
     elif natives_traveled >= (miles_traveled-15):
-        print("The natives are getting close!")
+        print("The Imperials are close!")
 
     if random.randrange(1,21) == 1:
-        print("You found an oasis!")
+        print("You a Cantina!")
         drinks = 6
         thirst = 0
         camel_tiredness = 0
